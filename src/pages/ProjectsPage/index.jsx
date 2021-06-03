9619b1f675ed8e91fc95ff647735fdb383d47150
nijin26 <nijinazarkpz@gmail.com>
2021-06-03 14:49:59 +0530

    Projects Page Completed

--- src/pages/ProjectsPage/index.jsx
+++ src/pages/ProjectsPage/index.jsx
@@ -2,7 +2,18 @@ import React, { useState, useEffect } from "react";
 
 //Importing Material UI stuff
 import { makeStyles } from "@material-ui/core/styles";
-import { Grid, Typography } from "@material-ui/core";
+import {
+  Grid,
+  Box,
+  Typography,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+} from "@material-ui/core";
+
+//Image Imports
+import cardMeda from "../../assets/projects-page/Rectangle 172.png";
 
 import style from "./style";
 const useStyles = makeStyles(style);
@@ -32,6 +43,248 @@ const ProjectsPage = () => {
           aliquip ex ea commodo consequat. Duis aute irure dolor laboris nisi ut
           aliquip ex ea commodo consequat. Duis aute irure dolor
         </Typography>
+      </Grid>
+      <Grid style={{ marginBottom: 20 }}>
+        <Box className={classes.cardSection}>
+          <Box className={classes.cardWrapper}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={cardMeda}
+                  title="Project 1"
+                />
+                <CardContent style={{ padding: "5px 0" }}>
+                  <Typography gutterBottom variant="h5" component="h4">
+                    Website Name
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    Website
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            <Typography
+              variant="h2"
+              className={`${classes.cardNum} ${classes.title} ${classes.num}`}
+            >
+              01
+            </Typography>
+          </Box>
+          <Box className={`${classes.cardWrapper} ${classes.alignedCard}`}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={cardMeda}
+                  title="Project 1"
+                />
+                <CardContent style={{ padding: "5px 0" }}>
+                  <Typography gutterBottom variant="h5" component="h4">
+                    Website Name
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    Website
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            <Typography
+              variant="h2"
+              className={`${classes.cardNum} ${classes.title} ${classes.num}`}
+            >
+              02
+            </Typography>
+          </Box>
+        </Box>
+        <Box className={classes.cardSection}>
+          <Box className={classes.cardWrapper}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={cardMeda}
+                  title="Project 1"
+                />
+                <CardContent style={{ padding: "5px 0" }}>
+                  <Typography gutterBottom variant="h5" component="h4">
+                    Website Name
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    Website
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            <Typography
+              variant="h2"
+              className={`${classes.cardNum} ${classes.title} ${classes.num}`}
+            >
+              03
+            </Typography>
+          </Box>
+          <Box className={`${classes.cardWrapper} ${classes.alignedCard}`}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={cardMeda}
+                  title="Project 1"
+                />
+                <CardContent style={{ padding: "5px 0" }}>
+                  <Typography gutterBottom variant="h5" component="h4">
+                    Website Name
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    Website
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            <Typography
+              variant="h2"
+              className={`${classes.cardNum} ${classes.title} ${classes.num}`}
+            >
+              04
+            </Typography>
+          </Box>
+        </Box>
+        <Box className={classes.cardSection}>
+          <Box className={classes.cardWrapper}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={cardMeda}
+                  title="Project 1"
+                />
+                <CardContent style={{ padding: "5px 0" }}>
+                  <Typography gutterBottom variant="h5" component="h4">
+                    Website Name
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    Website
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            <Typography
+              variant="h2"
+              className={`${classes.cardNum} ${classes.title} ${classes.num}`}
+            >
+              05
+            </Typography>
+          </Box>
+          <Box className={`${classes.cardWrapper} ${classes.alignedCard}`}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={cardMeda}
+                  title="Project 1"
+                />
+                <CardContent style={{ padding: "5px 0" }}>
+                  <Typography gutterBottom variant="h5" component="h4">
+                    Website Name
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    Website
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            <Typography
+              variant="h2"
+              className={`${classes.cardNum} ${classes.title} ${classes.num}`}
+            >
+              06
+            </Typography>
+          </Box>
+        </Box>
+        <Box className={classes.cardSection}>
+          <Box className={classes.cardWrapper}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={cardMeda}
+                  title="Project 1"
+                />
+                <CardContent style={{ padding: "5px 0" }}>
+                  <Typography gutterBottom variant="h5" component="h4">
+                    Website Name
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    Website
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            <Typography
+              variant="h2"
+              className={`${classes.cardNum} ${classes.title} ${classes.num}`}
+            >
+              07
+            </Typography>
+          </Box>
+          <Box className={`${classes.cardWrapper} ${classes.alignedCard}`}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={cardMeda}
+                  title="Project 1"
+                />
+                <CardContent style={{ padding: "5px 0" }}>
+                  <Typography gutterBottom variant="h5" component="h4">
+                    Website Name
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    Website
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            <Typography
+              variant="h2"
+              className={`${classes.cardNum} ${classes.title} ${classes.num}`}
+            >
+              08
+            </Typography>
+          </Box>
+        </Box>
       </Grid>
     </div>
   );
--- src/pages/ProjectsPage/style.js
+++ src/pages/ProjectsPage/style.js
@@ -9,6 +9,9 @@ export default function styles(theme) {
         "transparent" /*Will override color (regardless of order) */,
       " -webkit-text-stroke": " .1px #B6CFFF",
     },
+    num: {
+      fontSize: "4rem",
+    },
     section: {
       alignItems: "center",
       justifyContent: "center",
@@ -20,9 +23,54 @@ export default function styles(theme) {
       fontSize: "1rem",
       textAlign: "center",
       color: "#63718A",
+      marginBottom: "10%",
       [theme.breakpoints.down("sm")]: {
         padding: "0px 10% 0px 0px",
       },
+    },
+    cardSection: {
+      maxWidth: "100vw",
+      height: "80vh",
+      display: "flex",
+      flexDirection: "row",
+      flexWrap: "wrap",
+      justifyContent: "space-between",
+      [theme.breakpoints.down("sm")]: {
+        height: "120vh",
+        alignContent: "center",
+      },
+    },
+    card: {
+      position: "relative",
+      maxWidth: 461,
+      height: "max-content",
+      backgroundColor: "transparent",
+    },
+    media: {
+      maxWidth: 461,
+      height: 250,
+      background:
+        "linear-gradient(115.68deg, rgba(0, 0, 0, 0.3) 0%, rgba(93, 95, 239, 0.3) 100%), 0deg, #000000, #000000",
+    },
+    cardNum: {
+      position: "absolute",
+      top: -40,
+      left: 10,
+      [theme.breakpoints.down("sm")]: {
+        top: -38,
+        left: 8,
+      },
+    },
+    cardWrapper: {
+      height: "max-content",
+      width: 461,
+      position: "relative",
+      [theme.breakpoints.down("sm")]: {
+        marginTop: "40px",
+      },
+    },
+    alignedCard: {
+      alignSelf: "center",
     },
   };
 }
